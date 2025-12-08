87d8fec2e384f011e5b612798cb66b9abfa27b2d
Tejas Mankar <tejasmankar47@gmail.com>
2025-12-08 17:21:21 +0530

    RAG Used

---
+++ RAG/index.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_qdrant import QdrantVectorStore
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+pdf_path = Path(__file__).parent / "AI.pdf" 
+
+#######load the file using python
+loader = PyPDFLoader(file_path=pdf_path)
+docs = loader.load()
+
+
+########chunking spiliting the docs into smaller chunks
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 1000,
+    chunk_overlap = 400
+)
+
+chunks = text_splitter.split_documents(documents=docs)
+
+######## Vector embedding 
+embedding_model = OpenAIEmbeddings(
+    model= "text-embedding-3-large"
+)
+
+vector_store = QdrantVectorStore.from_documents(
+    documents=chunks,
+    embedding=embedding_model,
+    url="http://localhost:6333",
+    collection_name = "learning_rag"
+)
+
+print("Indexing of documents done....")
