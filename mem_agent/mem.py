59096346e30231a264d2a6f59f64432c59684314
Tejas Mankar <tejasmankar47@gmail.com>
2025-12-10 20:52:24 +0530

    Memory in LLMs

---
+++ mem_agent/mem.py
@@ -0,0 +1,70 @@
+from mem0 import Memory
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+load_dotenv()
+import json
+
+client = OpenAI()
+OPEN_API_KEY = os.getenv("OPEN_API_KEY")
+config = {
+    "version": "v1.1",
+    "embedder" : {
+        "provider" : "openai",
+        "config" : {"api_key" : OPEN_API_KEY, "model" : "text-embedding-3-small"}
+    },
+    "llm" : {
+        "provider" : "openai",
+        "config" : {"api_key" : OPEN_API_KEY, "model" : "gpt-4.1"}
+    },
+    "vector_store" : {
+        "provider" : "qdrant",
+        "config" : {
+            "host" : "localhost",
+            "port" : 6333
+        }
+    }
+}
+
+
+mem_client = Memory.from_config(config)
+
+
+while True:
+
+    user_query = input("> ")
+
+    search_memory = mem_client.search(query=user_query,user_id="Tejas")
+
+    memories = [
+        f"ID: {mem.get("id")}\nMemory: {mem.get("memory")}"
+        for mem in search_memory.get("results")
+    ]
+    print("Found Memories:" , memories)
+
+    SYSTEM_PROMPT = f"""
+        Here is the context about the user:
+        {json.dumps(memories)} 
+    """
+
+    response = client.chat.completions.create(
+        model="gpt-4.1-mini",
+        messages=[
+            {"role" : "system", "content" : SYSTEM_PROMPT},
+            {"role" : "user" , "content" : user_query}
+        ]
+    )
+
+    ai_response = response.choices[0].message.content
+
+    print("AI: ",ai_response)
+
+    mem_client.add(
+        user_id="Tejas",
+        messages=[
+            {"role" : "user", "content" : user_query},
+            {"role" : "assistant", "content" : ai_response}
+        ]
+    )
+
+    print("Memoruy has been saved....")
